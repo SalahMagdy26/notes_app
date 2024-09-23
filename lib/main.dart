065eed27dca6cae601c4714c5cfcae3aa2caf135
DESKTOP-V770RIF\SALAHMAGDY <salahmagdy2588@gmail.com>
2024-09-23 16:39:54 +0300

    adding hive package and app icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
-
+import 'package:hive_flutter/hive_flutter.dart';
+import 'constants/constants.dart';
 import 'views/note_view.dart';
 
-void main() {
+void main()async {
+
+  await Hive.initFlutter();
+  await Hive.openBox(kNoteBox);
   runApp(const NotesApp());
 }
 
